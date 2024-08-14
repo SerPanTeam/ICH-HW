2d74c7af3e907d7cfdf89e641fe2c6ffc55d255d
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-08-14 16:03:17 +0200

    ADD: new post - save and show

--- 13.1/script.js
+++ 13.1/script.js
@@ -1,14 +1,37 @@
-let posts = [];
 function sendPost(event) {
   event.preventDefault();
-  let postText = document.querySelector("#textPost");
-  document.body.appendChild(document.createElement("div.text")).textContent =
-    postText.value;
-  posts.push({
-    id: Date.now,
-    date: Date.toLocaleDateString(),
+  const postText = document.querySelector("#textPost");
+  post = {
+    id: Date.now(),
+    date: new Date().toLocaleDateString(),
     text: postText.value,
-  });
+  };
 
   postText.value = "";
+  localStorage.setItem(`post-${post.id}`, JSON.stringify(post));
+  showPost(post)
+}
+
+function renderPosts() {
+
+}
+
+function showPost(post) {
+  const postContainer = document.createElement("div");
+  postContainer.classList.add("post");
+
+  const postText = document.createElement("div");
+  postText.classList.add("text");
+  postText.textContent = post.text;
+
+  const postDate = document.createElement("div");
+  postDate.classList.add("date");
+  postDate.textContent = post.date;
+  
+  const postID = document.createElement("div");
+  postID.classList.add("id");
+  postID.textContent = post.id;
+
+  document.body.appendChild(postContainer).append(postText, postDate, postID);
+
 }
